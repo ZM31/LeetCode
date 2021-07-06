08c79636557ce21438681b51e44aba0db961a7c4
ZM31 <1744930859@qq.com>
2021-07-06 20:18:26 +0800

    20210706

---
+++ test.java
@@ -0,0 +1,17 @@
+import static java.lang.Character.getType;
+
+
+//import LeetCode106;
+public class test {
+    public static void main(String[] args) {
+//        TreeNode T ;
+//        int a[]={9,3,15,20,7};
+//        int b[] = {9,15,7,20,3};
+//        T = LeetCode106.buildTree(a,b);
+//        TreeNode root =new TreeNode();
+//        System.out.println(root.val);
+//        System.out.println(root);
+
+    }
+
+}
